0b8a0e2970baaa0392484666f36e946fe2a651a9
YeVirgo <64259080+YeVirgo@users.noreply.github.com>
2020-10-22 11:09:03 +0800

    Add files via upload

---
+++ CommentController.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Service
+{
+    [Route("api/[controller]")]
+    public class CommentController : Controller
+    {
+        [HttpGet]
+        public ActionResult Get()
+        {
+            return Json(DAL.UserInfo.Instance.GetCount());
+        }
+        [HttpGet]
+        public ActionResult Get(int id)
+        {
+            return Json(DAL.Comment.Instance.GetWorkCount(id));
+        }
+        [HttpPost]  //分页获取全部作品评论数据
+        public ActionResult Post([FromBody] Model.Page page)
+        {
+            var result = DAL.Comment.Instance.GetPage(page);
+            if (result.Count()==0)
+               return Json(Result.Err("返回数据为0"));
+            else
+                return Json(Result.Ok(result));
+        }
+        [HttpPost("workPage")]  //分页获取某作品评论数据
+        public ActionResult getWorkPage([FromBody] Model.CommentPage page)
+        {
+            var result = DAL.Comment.Instance.GetWorkPage(page);
+            if (result.Count()==0)
+                return Json(Result.Err("放回数据为0"));
+            else
+                return Json(Result.Ok(result));
+        }
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                var n = DAL.Comment.Instance.Delete(id);
+                if (n != 0)
+                    return Json(Result.Ok("删除成功"));
+                else
+                    return Json(Result.Err("commentId不存在"));
+            }
+            catch(Exception ex)
+            {
+                return Json(Result.Err(ex.Message));
+            }
+        }
+        [HttpPost] 
+        public ActionResult Post([FromBody] Model.Comment comment)
+        {
+            comment.commentTime = DateTime.Now;
+            try
+            {
+                int n = DAL.Comment.Instance.Add(comment);
+                return Json(Result.Ok("发表评论成功",n));
+            }
+            catch (Exception ex)
+            {
+                if (ex.Message.ToLower().Contains("foreige key"))
+                    if (ex.Message.ToLower().Contains("username"))
+                        return Json(Result.Err("合法用户才能添加记录"));
+                    else
+                        return Json(Result.Err("评论所属作品不存在"));
+                else if (ex.Message.ToLower().Contains("null"))
+                    return Json(Result.Err("评论内容、作品ID、用户名不能为空"));
+                else
+                    return Json(Result.Err(ex.Message));
+            }
+        }
+    }
+}
